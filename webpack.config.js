941a7522b555b7b0e4a0f950c8c717088c208366
ENGİN GÖKMEN <engingokmen@ENGINs-MacBook-Pro.local>
2024-09-04 19:56:11 +0300

    html plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            template: './src/html/index.html',
         }),
     ],
     output: {
@@ -26,4 +26,5 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    devtool: 'inline-source-map',
 }
